eaa1f5d2e746b62e770a9ff7dadd895490003139
dtorresUSB <dtorres@usbbog.edu.co>
2024-03-14 11:30:05 -0500

    archivos parcial

--- Sesion9/punto3.py
+++ Sesion9/punto3.py
@@ -9,21 +9,17 @@ def matriz():
         a.append([])
         for j in range(col):
             a[i].append(r.randint(-10,10))
+        print(a[i])
     return a,row,col
 
 def transpuesta(a,row,col):
     b=[]
-
+    print('--------------')
     for i in range(col):
         b.append([])
         for j in range(row):
             b[i].append(a[j][i])
-    
-    for i in a:
-        print(i)
-    print('--------------')
-    for i in b:
-        print(i)
+        print(b[i])
 
 if __name__=="__main__":
     a,row,col=matriz()
